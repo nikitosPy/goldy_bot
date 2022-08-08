7620d291a13289a570f46ecc963fb1df87ecfe4d
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-08 17:44:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import disnake as discord
 import os
+
 prefix = ["gg.", "gg!", "gb.", "gb!"]
-token =  'OTk0MjA5MzkyNTY2MjEwNTYw.GCDu3r.0yElEovGc991fo64-dAONQa9qVVvIAJlDj9sPw'
+with open("token.txt") as f:
+        token =  f.read()
 intents = discord.Intents.all()
 logs_ch = 998598317774737409
-statuses = statuses = [
+statuses = [
         "Привет! Я GoldyBot || gg.help", 
         "Хм... Зачем ты это читаешь?", 
         "Факт Дня: У меня ежедневные обновления!", 
@@ -16,7 +18,8 @@ statuses = statuses = [
         "MrGoldy Не делал бота, лишь командовал"
         ]
 goldy = 902168323557589012
-
+bad = "сука тварь гнида лох"
+bad_words = bad.split()
 cmds = []
 for filename in os.listdir("./commands"):
     if filename.endswith(".py"):
