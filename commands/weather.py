7332db3da6c941c65bc7bad7b33df40427886a81
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-10 20:19:54 +0300

    Update weather.py

--- commands/weather.py
+++ commands/weather.py
@@ -11,26 +11,26 @@ owm = OWM(owm_token, config_dict)
 mgr = owm.weather_manager()
 class Weather(commands.Cog):
   def __init__(self, bot):
-    	self.bot = bot
+      self.bot = bot
   @commands.command()
   async def weather(ctx: commands.Context, city: str): 
-  	observation = mgr.weather_at_place(city)
- 	w = observation.weather
-	t = w.temperature("celsius")
-	t1 = t['temp']
-  	t2 = t['feels_like']
-  	t3 = t['tem_max']
-	t4 = t['temp_min']
-	wi = w.wind()['speed']
-  	humi = w.humidity
-	cl = w.clouds
-	st = w.status
-	dt = w.detailed_status
-	ti = w.reference_time('iso')
-	pr = w.pressure['press']
-	vd = w.visibility_distance
+      observation = mgr.weather_at_place(city)
+      w = observation.weather
+      t = w.temperature("celsius")
+      t1 = t['temp']
+      t2 = t['feels_like']
+      t3 = t['tem_max']
+      t4 = t['temp_min']
+      wi = w.wind()['speed']
+      humi = w.humidity
+      cl = w.clouds
+      st = w.status
+      dt = w.detailed_status
+      ti = w.reference_time('iso')
+      pr = w.pressure['press']
+      vd = w.visibility_distance
 
-	await ctx.send("В городе " + str(place) + " температура " + str(t1) + " °C" + "\n" + 
+      await ctx.send("В городе " + str(place) + " температура " + str(t1) + " °C" + "\n" + 
 				"Максимальная температура " + str(t3) + " °C" +"\n" + 
 				"Минимальная температура " + str(t4) + " °C" + "\n" + 
 				"Ощущается как" + str(t2) + " °C" + "\n" +
