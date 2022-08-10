4acaaec4065327ce20ab72b505273e26727290c4
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-10 14:12:24 +0300

    Update weather.py

--- commands/weather.py
+++ commands/weather.py
@@ -9,22 +9,20 @@ class Weather(commands.Cog):
     self.bot = bot
   @commands.command()
   async def weather(ctx: commands.Context, city: str):
-     try:
-            
-            observation = mgr.weather_at_place(message.text)
-            w = observation.weather
-            oblaka = w.clouds
-            temp = w.temperature('celsius')["temp"]
-            vlazhnost = w.humidity
-
-            answer = 'В городе ' + message.text + ' сейчас ' + w.detailed_status + '\n'
-            answer += 'Температура ' + ' - ' + str(temp) + ' градусов' + '\n'
-            if temp < 10:
-                answer += 'Сейчас на улице очень холодно.Лучше сиди дома!'
-            if temp < 20:
-                answer += 'Воздух снаружи холодный.Одевайся теплее!'
-            else:
-                answer += 'Температура норм. Ходи в чем хочешь!'
-            await ctx.send(answer)
+    try:
+        observation = mgr.weather_at_place(message.text)
+        w = observation.weather
+        oblaka = w.clouds
+        temp = w.temperature('celsius')["temp"]
+        vlazhnost = w.humidity
+        answer = 'В городе ' + message.text + ' сейчас ' + w.detailed_status + '\n'
+        answer += 'Температура ' + ' - ' + str(temp) + ' градусов' + '\n'
+        if temp < 10:
+            answer += 'Сейчас на улице очень холодно.Лучше сиди дома!'
+        if temp < 20:
+            answer += 'Воздух снаружи холодный.Одевайся теплее!'
+        else:
+            answer += 'Температура норм. Ходи в чем хочешь!'
+        await ctx.send(answer)
     except:
-            await ctx.send("Такого города не существует.")
+        await ctx.send("Такого города не существует.")
