44d1e1ef66ba80f402a2e3d7173adc92b49adac1
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-11 12:51:06 +0300

    Update botinfo.py

--- commands/botinfo.py
+++ commands/botinfo.py
@@ -6,6 +6,9 @@ class Botinfo(commands.Cog):
         self.bot = bot
     @commands.command()
     async def botinfo(self, ctx: commands.Context):
-        await ctx.send(f"Информация о Боте {self.bot.user.name}! \nhttp://goldymine.tk \nВладельцы бота:  <@{goldy}> и <@{self.bot.owner.id}> \nБот создан: \nВ начале июля 2022 \nВ боте обновления каждый день!")
+        embed = discord.Embed(
+            title = f"Информация о Боте {self.bot.user.name}!"
+            description = f"http://goldymine.tk \nВладельцы бота:  <@{goldy}> и <@{self.bot.owner.id}> \nБот создан: \nВ начале июля 2022 \nВ боте обновления каждый день!"
+        await ctx.send(embed = embed)
 def setup(bot):
     bot.add_cog(Botinfo(bot))
