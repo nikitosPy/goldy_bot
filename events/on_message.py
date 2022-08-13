7514c0719dec294999a8f08547f340410ab76847
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-13 09:14:21 +0300

    Update on_message.py

--- events/on_message.py
+++ events/on_message.py
@@ -26,11 +26,12 @@ class Message(commands.Cog):
                 await message.channel.send(f"И тебе {wel}, дорогой <@{message.author.id}>!")
                 break
             for b in bad_words:
-                difference = process.extractOne(str(message.content).lower(), b)
-                if difference[1] >= 90:
-                    await message.channel.purge(limit = 1)
-                    await message.channel.send(f"<@{message.author.id}>, Не матерись! || Админы, он сказал: {message.content} ||")
-                    break
+                for content in str(message.content).lower().split():
+                    difference = process.extractOne(content, b)
+                    if difference[1] >= 90:
+                        await message.channel.purge(limit = 1)
+                        await message.channel.send(f"<@{message.author.id}>, Не матерись! || Админы, он сказал: {message.content} ||")
+                        break
         for ping in pings:
             if ping in str(message.content).lower():
                 await message.channel.purge(limit = 1)
