604b2395351d02dfa86dcbe88324456341a85825
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-14 14:02:26 +0300

    Update watch.py

--- commands/watch.py
+++ commands/watch.py
@@ -2,7 +2,8 @@ from distutils import extension
 import disnake as discord #Создание Клиента
 from disnake.utils import get #Поиск канала
 from disnake.ext import commands #Команды
-
+from config import *
+from discord_together import DiscprdTogether
 class Watch(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -10,6 +11,7 @@ class Watch(commands.Cog):
     async def watch(self, ctx: commands.Context):
         channel = ctx.author.voice.channel
         await channel.connect()
+        self.bot.togetherControl = await DiscordTogether(token)
         link = await self.bot.togetherControl.create_link(ctx.author.voice.channel.id, 'youtube')
         await ctx.send(f"Click the blue link!\n{link}")
 def setup(bot):
