f4ead897f19182c6fec9b8657a8d5e96cfe380ab
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-16 10:17:28 +0300

    Update joke.py

--- commands/joke.py
+++ commands/joke.py
@@ -2,6 +2,7 @@ import disnake as discord
 from disnake.ext import commands
 import json, aiohttp
 joke = 'https://geek-jokes.sameerkumar.website/api?format=json'
+from translate import Translator
 class Joke(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -11,6 +12,10 @@ class Joke(commands.Cog):
             async with cs.get(joke) as r:
                 if r.status == 200:
                     js = await r.json()
-                    await ctx.send(js['joke'])
+                    joke = js['joke']
+                    translator= Translator(to_lang="ru")
+                    joke = translator.translate(joke)
+                    await ctx.send(joke)
+                    
 def setup(bot):
     bot.add_cog(Joke(bot))
