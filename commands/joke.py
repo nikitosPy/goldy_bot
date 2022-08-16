4550fd16852af4e86643324c7c3faca76830374e
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-16 10:11:10 +0300

    Update joke.py

--- commands/joke.py
+++ commands/joke.py
@@ -1,13 +1,16 @@
 import disnake as discord
 from disnake.ext import commands
-from random import choice
-class Joke(commands.Cog):
+import json, aiohttp
+joke = ''
+class Cat(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     @commands.command()
-    async def joke(self, ctx: commands.Context):
-      with open('./jokes.txt') as j:
-          joke = choice(j.readLines())
-      await ctx.send(f'Случайная шутка: \n{joke}')
+    async def cat(self, ctx: commands.Context):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(joke) as r:
+                if r.status == 200:
+                    js = await r.json()
+                    await ctx.send(js['joke'])
 def setup(bot):
-    bot.add_cog(Joke(bot))
+    bot.add_cog(Cat(bot))
