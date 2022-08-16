a9e690b33e8ae270ea751f831e45db5424d68a04
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-16 10:12:49 +0300

    Update joke.py

--- commands/joke.py
+++ commands/joke.py
@@ -2,15 +2,15 @@ import disnake as discord
 from disnake.ext import commands
 import json, aiohttp
 joke = 'https://geek-jokes.sameerkumar.website/api?format=json'
-class Cat(commands.Cog):
+class Joke(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     @commands.command()
-    async def cat(self, ctx: commands.Context):
+    async def joke(self, ctx: commands.Context):
         async with aiohttp.ClientSession() as cs:
             async with cs.get(joke) as r:
                 if r.status == 200:
                     js = await r.json()
                     await ctx.send(js['joke'])
 def setup(bot):
-    bot.add_cog(Cat(bot))
+    bot.add_cog(Joke(bot))
