78f8a02309ef062d42332ea48efeb1f077b981ae
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-16 10:03:58 +0300

    Create joke.py

---
+++ commands/joke.py
@@ -0,0 +1,13 @@
+import disnake as discord
+from disnake.ext import commands
+from random import choice
+class Joke(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command()
+    async def joke(self, ctx: commands.Context):
+      with open('jokes.txt') as j:
+          joke = choice(j.readLines())
+      await ctx.send(f'Случайная шутка: \n{joke}')
+def setup(bot):
+    bot.add_cog(Joke(bot))
