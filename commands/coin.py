584cd656414a6f2497c451b39c0250da0d58695e
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-24 09:25:52 +0300

    Update coin.py

--- commands/coin.py
+++ commands/coin.py
@@ -1,5 +1,6 @@
 ﻿import disnake as discord
 from disnake.ext import commands
+from config import *
 import random
 rand = random.randint(0,2)
 if rand == 0:
@@ -15,5 +16,6 @@ class Coin(commands.Cog):
     async def coin(self, ctx: commands.Context):
         await ctx.send(embed = discord.Embed(title = f"Я подбросил монетку и получил: {coin}"))
         await ctx.send(file = discord.File(file))
+        log("coin")
 def setup(bot):
     bot.add_cog(Coin(bot))
