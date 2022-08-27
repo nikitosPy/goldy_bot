9597237058ea28d380c8987a907f8619ecc280aa
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-08-27 13:26:28 +0300

    Update ecogoldy.py

--- ecogoldy/ecogoldy.py
+++ ecogoldy/ecogoldy.py
@@ -5,7 +5,7 @@ import sqlite3
 from config import *
 from Cybernator import Paginator as pag
 
-connection = sqlite3.connect('server.db', host = "localhost")
+connection = sqlite3.connect('server.db')
 cursor = connection.cursor()
 class EcoGoldy(commands.Cog):
     def __init__(self, bot):
