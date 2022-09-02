fbb3147df0e95803473b45b5b5765a0a7205970e
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-09-02 13:20:56 +0300

    Update and rename echo.py to botecho.py

--- commands/botecho.py
+++ commands/botecho.py
@@ -2,31 +2,27 @@ from distutils import extension
 import disnake as discord #Создание Клиента
 from disnake.ext import commands #Команды
 from config import *
-class Echo(commands.Cog):
+class BotEcho(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def echo(self, ctx: commands.Context, member, content):
+    async def botecho(self, ctx: commands.Context, member, *, content):
         
-        await ctx.message.delete()  # We don't want users to see who initiated the command, to make it more realistic :P
-        # We fetch the channel's webhooks.
+        await ctx.message.delete()
         channel = ctx.channel
         avatar_url = self.bot.user.avatar.url
         if not isinstance(channel, discord.TextChannel):
             return
         channel_webhooks = await channel.webhooks()
-        # We check if the bot's webhook already exists in the channel.
         for webhook in channel_webhooks:
-            # We will check if the creator of the webhook is the same as the bot, and if the name is the same.
             if webhook.user == self.bot.user and webhook.name == "Bot Webhook":
                 break
         else:
-            # If the webhook does not exist, it will be created.
             webhook = await channel.create_webhook(name="Bot Webhook")
         await webhook.send(
             content=content, username=member, avatar_url = avatar_url
         )
-        log("echo")
+        log("botecho")
 def setup(bot):
-    bot.add_cog(Echo(bot))
+    bot.add_cog(BotEcho(bot))
