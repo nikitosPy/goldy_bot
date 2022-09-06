7f8602354c728003d5250a68311c72164b2cc095
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-09-06 19:22:52 +0300

    Update random.py

--- commands/random.py
+++ commands/random.py
@@ -1,11 +1,13 @@
-import disnake as discord
-from disnake.ext import commands
+import discord
+from discord.ext import commands
 import random
+from config import *
 class Random(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     @commands.command()
     async def random(self, ctx: commands.Context):
         await ctx.send(f'Случайное число от 0 до 1000: \n{random.randint(0,1001)}')
+        log("random")
 def setup(bot):
     bot.add_cog(Random(bot))
