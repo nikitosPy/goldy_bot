8aa7a0009e3f029533ad385610d208e4fda28e31
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-09-06 19:17:28 +0300

    Update fox.py

--- commands/fox.py
+++ commands/fox.py
@@ -1,5 +1,5 @@
-import disnake as discord
-from disnake.ext import commands
+import discord
+from discord.ext import commands
 import json, aiohttp
 from config import *
 animal = 'https://some-random-api.ml/img/'
