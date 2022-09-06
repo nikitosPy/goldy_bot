7c53c64370c3f6b940d89dc6ff652736568753b8
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-09-06 19:06:21 +0300

    Update botinfo.py

--- commands/botinfo.py
+++ commands/botinfo.py
@@ -4,11 +4,11 @@ from config import *
 class Botinfo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    @commands.command()
+    @commands.hybrid_command(name = "botinfo", with_app_command = True)
     async def botinfo(self, ctx: commands.Context):
         embed = discord.Embed(
             title = f"Информация о Боте {self.bot.user.name}!",
-            description = f"http://goldymine.tk \nВладельцы бота:  <@{goldy}> и <@{self.bot.owner.id}> \nБот создан: \nВ начале июля 2022 \nВ боте обновления каждый день!")
+            description = f"http://goldybot.gq \nВладельцы бота:  <@{goldy}> и <@{self.bot.owner.id}> \nБот создан: \nВ начале июля 2022 \nВ боте обновления каждый день!")
         await ctx.send(embed = embed)
         log("botinfo")
 def setup(bot):
