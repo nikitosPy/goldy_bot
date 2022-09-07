71c5e3e1f566056b09e3692e9addd149c566ff68
nikitosPy <108078315+nikitosPy@users.noreply.github.com>
2022-09-07 17:11:59 +0300

    Update role.py

--- commands/role.py
+++ commands/role.py
@@ -1,5 +1,5 @@
-import disnake as discord
-from disnake.ext import commands
+import discord
+from discord.ext import commands
 from asyncio import sleep
 from random import randint as rand
 class Role(commands.Cog):
