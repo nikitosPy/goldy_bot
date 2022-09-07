4e9dfe821acdad2419f5797f91fbc75cc661bd73
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-07 18:09:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,17 +24,19 @@ handler = logging.FileHandler(filename='disnake.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 async def load_extensions():
+    """
     for filename in os.listdir("./commands"):
         if filename.endswith(".py"):
             await bot.load_extension(f"commands.{filename[:-3]}")
-
+ """
     for filename in os.listdir("./events"):
         if filename.endswith(".py"):
             await bot.load_extension(f"events.{filename[:-3]}")
-
+"""
     for filename in os.listdir("./ecogoldy"):
         if filename.endswith(".py"):
             await bot.load_extension(f"ecogoldy.{filename[:-3]}")
+            """
 #Reload
 @bot.command()
 async def reload(ctx, extension):
