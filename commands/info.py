e50dea01ce5426dcb3402143ff11eb2d91e3a4f0
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-13 09:04:41 +0300

    Update info.py

--- commands/info.py
+++ commands/info.py
@@ -10,6 +10,10 @@ class Info(commands.Cog):
             title = f"Информация о Боте {self.bot.user.name}!",
             description = f"http://goldybot.gq \nВладельцы бота:  <@{goldy}> и <@{self.bot.owner.id}> \nБот создан: \nВ начале июля 2022 \nВ боте обновления каждый день!")
         await ctx.send(embed = embed)
+    @commands.hybrid_command(with_app_command = True)
+    async def commands(self, ctx: commands.Context):
+        cmnds = '\n- '.join([c.name for c in self.bot.commands])
+        await ctx.send(f"```\n{cmnds}\n```")
     @commands.command()
     async def ping(self, ctx: commands.Context):
         await ctx.channel.purge(limit = 1)
