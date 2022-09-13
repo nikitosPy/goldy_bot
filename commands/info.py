0fc65dd272661f9474e3154a824ea94780d79b1b
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-13 09:01:21 +0300

    Update info.py

--- commands/info.py
+++ commands/info.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from config import *
-class Botinfo(commands.Cog):
+class Info(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     @commands.hybrid_command(with_app_command = True)
@@ -10,6 +10,9 @@ class Botinfo(commands.Cog):
             title = f"Информация о Боте {self.bot.user.name}!",
             description = f"http://goldybot.gq \nВладельцы бота:  <@{goldy}> и <@{self.bot.owner.id}> \nБот создан: \nВ начале июля 2022 \nВ боте обновления каждый день!")
         await ctx.send(embed = embed)
-    @
+    @commands.command()
+    async def ping(self, ctx: commands.Context):
+        await ctx.channel.purge(limit = 1)
+        await ctx.send(f'Задержка бота: {round(self.bot.latency*1000)/1000} секунд')
 async def setup(bot):
-    await bot.add_cog(Botinfo(bot))
+    await bot.add_cog(Info(bot))
