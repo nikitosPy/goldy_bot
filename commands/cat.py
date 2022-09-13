23a4b27daa68344fea3b9ee6f725d88aee151b6b
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-13 08:42:41 +0300

    Update cat.py

--- commands/cat.py
+++ commands/cat.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import json, aiohttp
 from config import *
 animal = 'https://some-random-api.ml/img/'
-class Cat(commands.Cog):
+class Randomizer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     @commands.command()
@@ -13,6 +13,12 @@ class Cat(commands.Cog):
                 if r.status == 200:
                     js = await r.json()
                     await ctx.send(js['link'])
-                    log("cat")
+    @commands.command()
+    async def dog(self, ctx: commands.Context):
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(animal + 'dog') as r:
+                if r.status == 200:
+                    js = await r.json()
+                    await ctx.send(js['link'])
 def setup(bot):
-    bot.add_cog(Cat(bot))
+    bot.add_cog(Randomizer(bot))
