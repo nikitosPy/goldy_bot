b648a26680477dc5f37c3e50884fdf0344fd16b1
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-13 08:56:55 +0300

    Update joke.py

--- commands/joke.py
+++ commands/joke.py
@@ -19,7 +19,17 @@ class Fun(commands.Cog):
     @commands.command()
     async def random(self, ctx: commands.Context):
         await ctx.send(f'Случайное число от 0 до 1000: \n{random.randint(0,1001)}')
-    
+    @commands.command()
+    async def role(self, ctx: commands.Context):
+        role = discord.utils.get(ctx.guild.roles,name=ctx.author.name) 
+        if role not in ctx.guild.roles:
+            await ctx.guild.create_role(name = ctx.author.name)
+        role = discord.utils.get(ctx.guild.roles, name = ctx.author.name)
+        await role.edit(colour=discord.Color.random())
+        await ctx.message.author.add_roles(role) 
+        await sleep(300)
+        await ctx.message.author.remove_roles(role)
+        await role.delete()
     @commands.command()
     async def coin(self, ctx: commands.Context):
         rand = random.randrange(0,2)
