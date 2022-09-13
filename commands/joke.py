6154a8f720b6e88084d021f82693b49846f6670a
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-13 08:55:21 +0300

    Update joke.py

--- commands/joke.py
+++ commands/joke.py
@@ -19,12 +19,17 @@ class Fun(commands.Cog):
     @commands.command()
     async def random(self, ctx: commands.Context):
         await ctx.send(f'Случайное число от 0 до 1000: \n{random.randint(0,1001)}')
-    rand = random.randrange(0,2)
-    if rand == 0:
-        coin = "Орёл"
-        file = "coin1.jpg"
-    else:
-        coin = "Решка"
-        file = "coin2.jpg"
+    
+    @commands.command()
+    async def coin(self, ctx: commands.Context):
+        rand = random.randrange(0,2)
+        if rand == 0:
+            coin = "Орёл"
+            file = "coin1.jpg"
+        else:
+            coin = "Решка"
+            file = "coin2.jpg"
+        await ctx.send(embed = discord.Embed(title = f"Я подбросил монетку и получил: {coin}"))
+        await ctx.send(file = discord.File(file))
 def setup(bot):
     bot.add_cog(Fun(bot))
