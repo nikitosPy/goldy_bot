5eb934668f01daeddc3a8e4524afa67a953ac37f
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-16 19:34:00 +0300

    Update view_help.py

--- view_help.py
+++ view_help.py
@@ -4,20 +4,20 @@ from discord.ext import commands
 class Select(discord.ui.Select):
     def __init__(self):
         options=[
-            discord.SelectOption(label="Весёлости",emoji="😜",description="This is option 1!"),
-            discord.SelectOption(label="Полезные Функции",emoji="🤔",description="This is option 2!"),
-            discord.SelectOption(label="Экономика",emoji= "💰",description="This is option 3!")
+            discord.SelectOption(label="Весёлости",emoji="😜",description="Ура! Весело!"),
+            discord.SelectOption(label="Полезные Функции",emoji="🤔",description="Очень полезные!"),
+            discord.SelectOption(label="Экономика",emoji= "💰",description="Команды для бизнеса!")
             ]
         super().__init__(placeholder="Select an option",max_values=1,min_values=1,options=options)
     async def callback(self, interaction: discord.Interaction):
-        if self.values[0] == "Option 1":
-            await interaction.response.edit_message(content="This is the first option from the entire list!")
-        elif self.values[0] == "Option 2":
-            await interaction.response.send_message("This is the second option from the list entire wooo!",ephemeral=False)
-        elif self.values[0] == "Option 3":
-            await interaction.response.send_message("Third One!",ephemeral=True)
+        if self.values[0] == "Весёлости":
+            await interaction.response.send_message("Команды веселья!", ephemeral=True)
+        elif self.values[0] == "Полезные функции":
+            await interaction.response.send_message("Полезные функции!",ephemeral=True)
+        elif self.values[0] == "Экономика":
+            await interaction.response.send_message("Бизнес!",ephemeral=True)
 
 class SelectView(discord.ui.View):
-    def __init__(self, *, timeout = 180):
+    def __init__(self, *, timeout = 100):
         super().__init__(timeout=timeout)
         self.add_item(Select())
