602b0c3d8a6708f812c01c3135713a891502eefd
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-17 09:24:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ print("Запуск...")
 try:
     import discord #Создание Клиента
     from discord.ext import commands #Команды
+    from enum import Enum
     import pretty_errors
     from typing import List
     import asyncio
@@ -64,10 +65,64 @@ async def reload(ctx):
         await ctx.send("Не похож ты на моего разработчика...")
 #
 
+connections = {}
 
-        
-with open("logs.txt", "a+") as f:
-    f.write("\n")           
+
+class Sinks(Enum):
+    mp3 = discord.sinks.MP3Sink()
+    wav = discord.sinks.WaveSink()
+    pcm = discord.sinks.PCMSink()
+    ogg = discord.sinks.OGGSink()
+    mka = discord.sinks.MKASink()
+    mkv = discord.sinks.MKVSink()
+    mp4 = discord.sinks.MP4Sink()
+    m4a = discord.sinks.M4ASink()
+
+
+async def finished_callback(sink, channel: discord.TextChannel, *args):
+    recorded_users = [f"<@{user_id}>" for user_id, audio in sink.audio_data.items()]
+    await sink.vc.disconnect()
+    files = [
+        discord.File(audio.file, f"{user_id}.{sink.encoding}")
+        for user_id, audio in sink.audio_data.items()
+    ]
+    await channel.send(
+        f"Finished! Recorded audio for {', '.join(recorded_users)}.", files=files
+    )
+
+
+@bot.command()
+async def start(ctx: discord.ApplicationContext, sink: Sinks):
+    """
+    Record your voice!
+    """
+    voice = ctx.author.voice
+
+    if not voice:
+        return await ctx.respond("You're not in a vc right now")
+
+    vc = await voice.channel.connect()
+    connections.update({ctx.guild.id: vc})
+
+    vc.start_recording(
+        sink.value,
+        finished_callback,
+        ctx.channel,
+    )
+
+    await ctx.respond("The recording has started!")
+
+
+@bot.command()
+async def stop(ctx: discord.ApplicationContext):
+    """Stop recording."""
+    if ctx.guild.id in connections:
+        vc = connections[ctx.guild.id]
+        vc.stop_recording()
+        del connections[ctx.guild.id]
+        await ctx.delete()
+    else:
+        await ctx.respond("Not recording in this guild.")
 
 bot.run(token)
 
