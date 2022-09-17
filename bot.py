d2ec8a638edb991d88b21b76e94f1798f057f4fc
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-17 12:16:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,65 +65,6 @@ async def reload(ctx):
         await ctx.send("Не похож ты на моего разработчика...")
 #
 
-connections = {}
-
-
-class Sinks(Enum):
-    mp3 = discord.sinks.MP3Sink()
-    wav = discord.sinks.WaveSink()
-    pcm = discord.sinks.PCMSink()
-    ogg = discord.sinks.OGGSink()
-    mka = discord.sinks.MKASink()
-    mkv = discord.sinks.MKVSink()
-    mp4 = discord.sinks.MP4Sink()
-    m4a = discord.sinks.M4ASink()
-
-
-async def finished_callback(sink, channel: discord.TextChannel, *args):
-    recorded_users = [f"<@{user_id}>" for user_id, audio in sink.audio_data.items()]
-    await sink.vc.disconnect()
-    files = [
-        discord.File(audio.file, f"{user_id}.{sink.encoding}")
-        for user_id, audio in sink.audio_data.items()
-    ]
-    await channel.send(
-        f"Finished! Recorded audio for {', '.join(recorded_users)}.", files=files
-    )
-
-
-@bot.command()
-async def start(ctx):
-    """
-    Record your voice!
-    """
-    voice = ctx.author.voice
-
-    if not voice:
-        return await ctx.send("You're not in a vc right now")
-
-    vc = await voice.channel.connect()
-    connections.update({ctx.guild.id: vc})
-
-    vc.start_recording(
-        discord.sinks.MP3Sink(),
-        finished_callback,
-        ctx.channel,
-    )
-
-    await ctx.send("The recording has started!")
-
-
-@bot.command()
-async def stop(ctx):
-    """Stop recording."""
-    if ctx.guild.id in connections:
-        vc = connections[ctx.guild.id]
-        vc.stop_recording()
-        del connections[ctx.guild.id]
-        await ctx.delete()
-    else:
-        await ctx.send("Not recording in this guild.")
-
 bot.run(token)
 
 
