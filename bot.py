1a4ff461325e39b15f2024006e3081c137fbe182
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-17 13:18:06 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,64 +39,18 @@ class GoldyBot(commands.Bot):
         await self.wait_until_ready()	
         load_extensions()
         abio = str(datetime.datetime.now())
+        authorizator =  f'Bot {token[:-2]'
         requests.patch(url="https://discord.com/api/v9/users/@me", 
-                       headers= {"authorization": f'Bot {token[:-2]}'}, 
+                       headers= {"authorization": authorizator,
+                                "Content-Type": "application/json",}, 
                        json = {"bio": abio} )
-        await self.user.edit(username = f'[BOT]{self.user.name}')
+        await self.user.edit(username = f'GoldyBot')
         print("ready!")
     async def setup_hook(self):
         try:
             await self.tree.sync()
         except:
             print('sync')
-            
-ytdl_format_options = {
-    "format": "bestaudio/best",
-    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
-    "restrictfilenames": True,
-    "noplaylist": True,
-    "nocheckcertificate": True,
-    "ignoreerrors": False,
-    "logtostderr": False,
-    "quiet": True,
-    "no_warnings": True,
-    "default_search": "auto",
-    "source_address": "0.0.0.0",  # Bind to ipv4 since ipv6 addresses cause issues at certain times
-}
-
-ffmpeg_options = {"options": "-vn"}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source: discord.AudioSource, *, data: dict, volume: float = 0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get("title")
-        self.url = data.get("url")
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if "entries" in data:
-            # Takes the first item from a playlist
-            data = data["entries"][0]
-
-        filename = data["url"] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)            
-            
-            
-            
-            
-            
-            
-            
-            
             
 bot = GoldyBot()
 bot.remove_command('help')
@@ -108,17 +62,6 @@ logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='disnake.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
-
-@bot.command()
-async def play(ctx, channel: discord.VoiceChannel, *, url: str):
-    await channel.connect()
-    async with ctx.typing():
-        player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
-        ctx.voice_client.play(player, after=lambda e: print(f"Player error: {e}") if e else None)
-    await ctx.send(f"Now playing: {player.title}")
-
-
-
 
 #Reload
 @bot.command()
