699ff7b8fb1a290503dc7411ee6a517859a182a4
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-17 12:36:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -102,13 +102,13 @@ handler = logging.FileHandler(filename='disnake.log', encoding='utf-8', mode='w'
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
-@commands.command()
-async def play(ctx, *, url: str):
+@bot.command()
+async def play(ctx, channel: discord.VoiceChannel, *, url: str):
+    await channel.connect()
     async with ctx.typing():
         player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
         ctx.voice_client.play(player, after=lambda e: print(f"Player error: {e}") if e else None)
     await ctx.send(f"Now playing: {player.title}")
-
 
 
 
