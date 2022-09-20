34cd83f102b3d39113dbaf3f0700fd774f189ba3
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-20 20:03:51 +0300

    Update fun.py

--- commands/fun.py
+++ commands/fun.py
@@ -43,8 +43,30 @@ class Fun(commands.Cog):
         await ctx.send(embed = discord.Embed(title = f"Я подбросил монетку и получил: {coin}"))
         await ctx.send(file = discord.File(file))
         
-        
-        
+    @commands.command()
+    async def user(self, ctx: commands.Context):
+        background = Editor("assets/wlcbg.jpg")
+        profile_image = load_image(str(ctx.author.avatar_url))
+        profile = Editor(profile_image).resize((150, 150)).circle_image()
+
+        poppins = Font.poppins(size=50, variant="bold")
+        poppins_small = Font.poppins(size=25, variant="regular")
+        poppins_light = Font.poppins(size=20, variant="light")
+
+        background.paste(profile, (325, 90))
+        background.ellipse((325, 90), 150, 150, outline="gold", stroke_width=4)
+        background.text((400, 260), "WELCOME", color="white", font=poppins, align="center")
+        background.text(
+    (400, 325), "Shahriyar#9770", color="white", font=poppins_small, align="center"
+)
+        background.text(
+    (400, 360),
+    "You are the 457th Member",
+    color="#0BE7F5",
+    font=poppins_small,
+    align="center",
+)
+        awwit ctx.send(file = discord.File(fp=background)
         
 def setup(bot):
     bot.add_cog(Fun(bot))
