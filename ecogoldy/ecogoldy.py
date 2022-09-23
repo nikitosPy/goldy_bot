f21ecaf37737d2305eb1ec32393ef0be3ab0a708
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-23 08:18:42 +0300

    Update ecogoldy.py

--- ecogoldy/ecogoldy.py
+++ ecogoldy/ecogoldy.py
@@ -13,6 +13,7 @@ class EcoGoldy(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
+    @commands.Cog.listener('on_ready')
     async def on_ready(self):
         cursor.execute("""CREATE TABLE IF NOT EXISTS users (
             name TEXT,
