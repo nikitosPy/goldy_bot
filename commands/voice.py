df3aaa5f19a7350f214fbc16c11d61efe5cce862
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-09-28 21:53:09 +0300

    Update voice.py

--- commands/voice.py
+++ commands/voice.py
@@ -2,12 +2,14 @@ import discord
 from discord.ext.commands import Context, Cog
 from discord.ext import commands, bridge
 from gtts import gTTS
+from os.path import exists
 class Voice(Cog):
   def __init__(self, bot):
     self.bot = bot
   @bot.command()
   async def play (ctx, *, text1):    
-      os.remove('text.mp3')
+      if exists('text.mp3'):
+        os.remove('text.mp3')
       tts = gTTS(text = text1, lang = 'ru')
       tts.save('text.mp3')
       voice_channel = ctx.author.channel
