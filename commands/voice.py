16b7a1ee3fecb9f0102dd10a9e25c3f66ebfe571
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-10-01 21:36:51 +0300

    Update voice.py

--- commands/voice.py
+++ commands/voice.py
@@ -7,6 +7,8 @@ from asyncio import sleep
 from os.path import exists
 from pathlib import Path
 import speech_recognition as sr
+import youtube_dl
+
 r = sr.Recognizer()
 connections = {}
 async def finished_callback(sink, channel: discord.TextChannel, *args):
@@ -24,7 +26,57 @@ async def finished_callback(sink, channel: discord.TextChannel, *args):
         audio = r.record(source)
     text = r.recognize_google(audio)
     await channel.send(str(text))
+youtube_dl.utils.bug_reports_message = lambda: ""
 
+
+ytdl_format_options = {
+    "format": "bestaudio/best",
+    "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
+    "restrictfilenames": True,
+    "noplaylist": True,
+    "nocheckcertificate": True,
+    "ignoreerrors": False,
+    "logtostderr": False,
+    "quiet": True,
+    "no_warnings": True,
+    "default_search": "auto",
+    "source_address": "0.0.0.0",  # Bind to ipv4 since ipv6 addresses cause issues at certain times
+}
+
+ffmpeg_options = {"options": "-vn"}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source: discord.AudioSource, *, data: dict, volume: float = 0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get("title")
+        self.url = data.get("url")
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if "entries" in data:
+            # Takes the first item from a playlist
+            data = data["entries"][0]
+
+        filename = data["url"] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+    
+    
+    
+    
+    
+    
+    
+    
+    
 class Voice(Cog):
   def __init__(self, bot):
     self.bot = bot
@@ -45,8 +97,8 @@ class Voice(Cog):
       audio = discord.FFmpegPCMAudio(source='text.mp3')
       vc.play(audio)
       while vc.is_playing():
-          sleep(.1)
-      await vc.disconnect()
+          await sleep(.1)
+      
       await ctx.send('Конец')
   
   @commands.command()
@@ -80,5 +132,15 @@ class Voice(Cog):
           await ctx.delete()
       else:
           await ctx.respond("Not recording in this guild.")
+            
+  @commands.command()
+    async def play(self, ctx: commands.Context, *, url: str):
+        """Streams from a url (same as yt, but doesn't predownload)"""
+
+        async with ctx.typing():
+            player = await YTDLSource.from_url(url, loop=self.bot.loop, stream=True)
+            ctx.voice_client.play(player, after=lambda e: print(f"Player error: {e}") if e else None)
+
+        await ctx.send(f"Now playing: {player.title}")
 def setup(bot):
   bot.add_cog(Voice(bot))
