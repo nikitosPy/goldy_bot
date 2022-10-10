1c607e727239a52b86319bd43d267563ee994045
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-10-10 20:57:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,14 @@
 
+import platform
 print("Запуск...") 
+print("="*10, "О системе: ", "="*10)
+uname = platform.uname()
+print(f"System: {uname.system}")
+print(f"Node Name: {uname.node}")
+print(f"Release: {uname.release}")
+print(f"Version: {uname.version}")
+print(f"Machine: {uname.machine}")
+print(f"Processor: {uname.processor}")
 ###
 try:
     import discord #Создание Клиента
