d322bde41dab40d530f70d871b606ad7d4bee7b3
AzertyBroGG <112948773+AzertyBroGG@users.noreply.github.com>
2022-10-15 20:42:54 +0300

    Create panel.py

---
+++ panel.py
@@ -0,0 +1,12 @@
+import discord
+from discord.ext import commands
+class Panel(discord.ui.Button):
+    def __init__(self, bot, *args, **kwargs):
+      self.bot = bo5
+      super().__init__(*args, **kwargs)
+    @discord.ui.button(label = 'botinfo', style = discord.ButtonStyle.primary
+    async def botinfo(self, button, interaction):
+        embed = discord.Embed(
+            title = f"Информация о Боте {self.bot.user.name}!",
+            description = f"http://goldybot.gq \nВладельцы бота:  <@{goldy}> и <@{self.bot.owner_id}> \nБот создан: \n{self.bot.user.created_at} \nВ боте обновления каждый день!")
+        await interaction.response.send(embed = embed)
